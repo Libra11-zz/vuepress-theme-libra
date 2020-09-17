664bb07a222544be7db3b384359ba12c4727410e
Libra <libra_085925@163.com>
2020-09-18 00:26:45 +0800

    fix mobile link bug

--- docs/.vuepress/theme/layouts/DirectoryPagination.vue
+++ docs/.vuepress/theme/layouts/DirectoryPagination.vue
@@ -36,6 +36,7 @@
             :title="item.frontmatter.title"
             :content="item.frontmatter.desc"
             :time="item.frontmatter.date"
+            :path="item.path"
             :category="item.frontmatter.category"
           />
         </div>
